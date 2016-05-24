7d81e9e235b88cc26533a068dc2f9f6d276d96d1
virtualpatterns <code.virtualpatterns@gmail.com>
2016-05-23 23:34:42 -0400

    Commenting push and release for now

--- task/index.js
+++ task/index.js
@@ -21,14 +21,45 @@ task('Log.addFile', function () {
   Log.addFile(LOG_PATH);
 });
 
-desc('Push to development');
-task('push', ['Log.addFile'], function () {
-  Task.createTask(this.name)
-    .add('git checkout development', Task.OPTIONS_STDIO_IGNORE)
-    .add('git pull origin development', Task.OPTIONS_STDIO_IGNORE)
-    .add('git push origin development', Task.OPTIONS_STDIO_IGNORE)
-    .add('git status')
-    .execute(complete, fail);
-});
+// desc('Push to development');
+// task('push', ['Log.addFile'], function () {
+//   Task.createTask(this.name)
+//     .add('git checkout development', Task.OPTIONS_STDIO_IGNORE)
+//     .add('git pull origin development', Task.OPTIONS_STDIO_IGNORE)
+//     .add('git push origin development', Task.OPTIONS_STDIO_IGNORE)
+//     .add('git status')
+//     .execute(complete, fail);
+// });
+
+// desc('Push to production, release, and increment version');
+// task('release', ['Log.addFile'], function () {
+//   Task.createTask(this.name)
+//     .add('git checkout production')
+//     .add('git pull origin production')
+//     .add('git merge origin development')
+//     .add('git push origin production')
+//     // .add('npm release')
+//     .add('git checkout development')
+//     .add('git status')
+//     .execute(complete, fail);
+// });
+
+// desc 'Push to production, release, and increment version'
+// task :release do |task|
+//     system('git checkout production; git pull origin production; git merge origin/development; git push origin production; rake release; git checkout development')
+//     version_file = File.join(RubyApp::ROOT, %w[version.rb])
+//     RubyApp::VERSION =~ /(\d+)\.(\d+)\.(\d+)/
+//     system("sed 's|[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*|#{$1}.#{$2}.#{$3.to_i + 1}|g' < '#{version_file}' > '#{version_file}.out'; rm '#{version_file}'; mv '#{version_file}.out' '#{version_file}'")
+//     system("git commit --all --message=\'Version #{$1}.#{$2}.#{$3.to_i + 1}\'")
+// end
+
+// desc 'Pull development, tag, push to development, and increment version'
+// task :push do |task|
+//   system("git checkout development; git pull origin development; git tag -a -m 'Tag #{Pike::VERSION}' '#{Pike::VERSION}'; git push --tags origin development")
+//   version_file = File.join(Pike::ROOT, %w[lib pike version.rb])
+//   Pike::VERSION =~ /(\d+)\.(\d+)\.(\d+)/
+//   system("sed 's|[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*|#{$1}.#{$2}.#{$3.to_i + 1}|g' < '#{version_file}' > '#{version_file}.out'; rm '#{version_file}'; mv '#{version_file}.out' '#{version_file}'")
+//   system("git commit --all --message='Version #{$1}.#{$2}.#{$3.to_i + 1}'")
+// end
 
 require('task/tasks/test')
