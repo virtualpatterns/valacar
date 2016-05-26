b341324284dd85a188aad6d32b28473560ab0c04
virtualpatterns <code.virtualpatterns@gmail.com>
2016-05-26 15:54:05 -0400

    Prepping for release

--- task/index.js
+++ task/index.js
@@ -40,14 +40,14 @@ task('push', ['log'], function (version) {
 desc('Release production');
 task('release', ['log'], function () {
   Task.createTask(this.name)
-    .add('%j', Path.join(RESOURCES_PATH, 'git-is-dirty.sh'))
-    .add('mocha --require test/index.js test/tests')
-    .add('git checkout production')
-    .add('git pull origin production')
-    .add('git merge origin development')
-    .add('git push origin production --tags')
-    .add('npm publish')
-    .add('git checkout development')
+    // .add('%j', Path.join(RESOURCES_PATH, 'git-is-dirty.sh'))
+    // .add('mocha --require test/index.js test/tests')
+    // .add('git checkout production')
+    // .add('git pull origin production')
+    // .add('git merge origin development')
+    // .add('git push origin production --tags')
+    // .add('npm publish')
+    // .add('git checkout development')
     .execute(complete, fail);
 });
 
