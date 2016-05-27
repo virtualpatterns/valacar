4be48bc2d3ce33f320f3e8063171020a5fa9d4ca
virtualpatterns <code.virtualpatterns@gmail.com>
2016-05-27 15:07:06 -0400

    Removing app-module-path, and incremented version

--- libraries.js
+++ libraries.js
@@ -2,20 +2,18 @@
 
 'use strict';
 
-require('./index');
-
 console.log('--- . --------------------------------------------------------------------------')
-console.log(require.resolve('library/errors/argument-error'));
-console.log(require.resolve('library/errors/migration-error'));
-console.log(require.resolve('library/errors/process-error'));
-console.log(require.resolve('library/errors/validation-error'));
-console.log(require.resolve('library/application'));
-console.log(require.resolve('library/database'));
-console.log(require.resolve('library/leases'));
-console.log(require.resolve('library/log'));
-console.log(require.resolve('library/migration'));
-console.log(require.resolve('library/path'));
-console.log(require.resolve('library/process'));
-console.log(require.resolve('package.json'));
+console.log(require.resolve('./library/errors/argument-error'));
+console.log(require.resolve('./library/errors/migration-error'));
+console.log(require.resolve('./library/errors/process-error'));
+console.log(require.resolve('./library/errors/validation-error'));
+console.log(require.resolve('./library/application'));
+console.log(require.resolve('./library/database'));
+console.log(require.resolve('./library/leases'));
+console.log(require.resolve('./library/log'));
+console.log(require.resolve('./library/migration'));
+console.log(require.resolve('./library/path'));
+console.log(require.resolve('./library/process'));
+console.log(require.resolve('./package.json'));
 
-require('library/libraries');
+require('./library/libraries');
--- library/errors/libraries.js
+++ library/errors/libraries.js
@@ -1,15 +1,15 @@
 'use strict';
 
-console.log('--- ./library/errors -----------------------------------------------------------')
-console.log(require.resolve('library/errors/argument-error'));
-console.log(require.resolve('library/errors/migration-error'));
-console.log(require.resolve('library/errors/process-error'));
-console.log(require.resolve('library/errors/validation-error'));
-console.log(require.resolve('library/application'));
-console.log(require.resolve('library/database'));
-console.log(require.resolve('library/leases'));
-console.log(require.resolve('library/log'));
-console.log(require.resolve('library/migration'));
-console.log(require.resolve('library/path'));
-console.log(require.resolve('library/process'));
-console.log(require.resolve('package.json'));
+console.log('--- ./. -----------------------------------------------------------')
+console.log(require.resolve('./argument-error'));
+console.log(require.resolve('./migration-error'));
+console.log(require.resolve('./process-error'));
+console.log(require.resolve('./validation-error'));
+console.log(require.resolve('../application'));
+console.log(require.resolve('../database'));
+console.log(require.resolve('../leases'));
+console.log(require.resolve('../log'));
+console.log(require.resolve('../migration'));
+console.log(require.resolve('../path'));
+console.log(require.resolve('../process'));
+console.log(require.resolve('../../package.json'));
